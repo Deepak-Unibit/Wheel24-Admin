878e479236718ba313a663bd2fdb8a8c2fd92e77
Deepak <deepak.unibit@gmail.com>
2024-10-15 18:41:44 +0530

    cdxfvxz

--- lib/api/app.const.dart
+++ lib/api/app.const.dart
@@ -2,9 +2,9 @@ class AppConst {
   static String authToken = '';
 
   // Development
-  // static const String baseUrl = "https://tgb-2.ludo24.app";
+  static const String baseUrl = "https://tgb-2.ludo24.app";
 
   // Production
-  static const String baseUrl = "https://dev.fun11.in:8005";
+  // static const String baseUrl = "https://dev.fun11.in:8005";
 
 }
