656f640c06a093c7fbe6f5fdb4886568e40a3e82
Deepak <deepak.unibit@gmail.com>
2024-10-15 18:19:39 +0530

    cdxfvxz

---
+++ lib/models/blockIP.model.dart
@@ -0,0 +1,38 @@
+class BlockIpModel {
+  BlockIpModel({
+      String? status, 
+      String? message, 
+      num? responseCode, 
+      bool? data,}){
+    _status = status;
+    _message = message;
+    _responseCode = responseCode;
+    _data = data;
+}
+
+  BlockIpModel.fromJson(dynamic json) {
+    _status = json['status'];
+    _message = json['message'];
+    _responseCode = json['responseCode'];
+    _data = json['data'];
+  }
+  String? _status;
+  String? _message;
+  num? _responseCode;
+  bool? _data;
+
+  String? get status => _status;
+  String? get message => _message;
+  num? get responseCode => _responseCode;
+  bool? get data => _data;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['status'] = _status;
+    map['message'] = _message;
+    map['responseCode'] = _responseCode;
+    map['data'] = _data;
+    return map;
+  }
+
+}
